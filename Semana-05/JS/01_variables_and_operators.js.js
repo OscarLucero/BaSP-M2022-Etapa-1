89867ccaa3d6447167f503713f55b4077f573dd9
Oscar Lucero <olucero921@gmail.com>
2022-04-19 10:10:06 -0300

    BaSP-2022: ex01-variables and operators

--- Semana-05/JS/01_variables_and_operators.js.js
+++ Semana-05/JS/01_variables_and_operators.js.js
@@ -3,18 +3,18 @@ n1 = 12;
 n2 = 36;
 
 var result = n1 + n2;
-console.log("A-VyO: " + result);
+console.log("ex01-variables and operators-A: " + result);
 
 var str1, str2, result;
 str1= "Ri"
 str2= "ver"
 var result = str1 + str2;
-console.log("B-VyO: " + result)
+console.log("ex02-variables and operators-B: " + result)
 
 var w1, w2;
 w1= "Gallardo"
 w2= "Millonario"
 var result = w1.length + w2.length;
 
-console.log("C-VyO: " + result)
+console.log("ex03-variables and operators-C: " + result)
 
