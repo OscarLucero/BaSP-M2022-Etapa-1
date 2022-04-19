153eede4cd32c85deaedf0415273bb18c212cd1f
Oscar Lucero <olucero921@gmail.com>
2022-04-19 10:03:14 -0300

    BaSP-2022: ex01-variables and operators

---
+++ Semana-05/JS/VyO.js
@@ -0,0 +1,20 @@
+var n1, n2; 
+n1 = 12;
+n2 = 36;
+
+var result = n1 + n2;
+console.log("A-VyO: " + result);
+
+var str1, str2, result;
+str1= "Ri"
+str2= "ver"
+var result = str1 + str2;
+console.log("B-VyO: " + result)
+
+var w1, w2;
+w1= "Gallardo"
+w2= "Millonario"
+var result = w1.length + w2.length;
+
+console.log("C-VyO: " + result)
+
